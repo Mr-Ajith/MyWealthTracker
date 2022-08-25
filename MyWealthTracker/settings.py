066aec4aed3a0b258721333062f7a713562b6248
Ajith <73734161+Mr-Ajith@users.noreply.github.com>
2022-08-25 22:22:23 +0530

    up

--- MyWealthTracker/settings.py
+++ MyWealthTracker/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 WHITENOISE_USE_FINDERS = True
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
